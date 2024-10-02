2b8e60477f5899284ece130ac8172e57b618007c
katherinerguez <rkatherine538@gmail.com>
2024-10-02 11:15:25 -0400

    add all models to visialization

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ import plotly.graph_objects as go
 import random
 from models.SIR_model import spreading_init, spreading_seed, spreading_make_sir_model, spreading_step
 from models.SIS_model import spreading_make_sis_model
+from models.SIRS_model_in_period import spreading_make_sirs_model
+from models.SIRS_model_psusceptible import spreading_make_sirs_model
+from models.SIRD_model import spreading_make_sird_model
+from models.SEIRS_model_pLossImmunity import spreading_make_seirs_model
+from models.SEIR_model import spreading_make_seir_model
+from models.SEIRS_model_immunity_period import spreading_make_seirs_model
 
 # Constants for SIR model states
 SPREADING_SUSCEPTIBLE = 'S'
@@ -13,13 +19,15 @@ SPREADING_INFECTED = 'I'
 SPREADING_RECOVERED = 'R'
 SPREADING_DEAD = 'D'
 SPREADING_QUARANTINED = 'Quarantined'
+SPREADING_EXPOSED = 'E'
 
 color_map = {
     SPREADING_SUSCEPTIBLE: 'blue',
     SPREADING_INFECTED: 'red',
     SPREADING_RECOVERED: 'green',
     SPREADING_DEAD: 'black',
-    SPREADING_QUARANTINED: 'orange'
+    SPREADING_QUARANTINED: 'orange',
+    SPREADING_EXPOSED:'yellow'
 }
 
 # Crear la aplicación Dash
@@ -33,7 +41,13 @@ app.layout = html.Div([
         id='model-dropdown',
         options=[
             {'label': 'SIR', 'value': 'SIR'},
+            {'label':'SIRD', 'value':'SIRD'},
             {'label': 'SIS', 'value': 'SIS'},
+            {'label': 'SIRS en un periodo de susceptibilidad', 'value':'SIRS_period'},
+            {'label': 'SIRS con una probabilidad de susceptibilidad','value':'SIRS_probability'},
+            {'label':'SEIR', 'value':'SEIR'},
+            {'label':'SEIRS con un periodo de inmunidad', 'value':'SEIRS_inmunity'},
+            {'label':'SEIRS plossimmunity', 'value':'SEIRS_plossimmunity'},
         ],
         value='SIR'
     ),
@@ -98,10 +112,112 @@ def display_model_parameters(selected_model):
             html.Label("Probabilidad de Ser Nuevamente Susceptible (pRecover):"),
             dcc.Input(id='pRecover', type='number', value=0.01, step=0.01),
         ])
+    elif selected_model == 'SIRD':
+        return html.Div([
+            html.Label("Número de Nodos:"),
+            dcc.Input(id='num-nodes', type='number', value=300, min=10, step=10),
+            
+            html.Label("Probabilidad de Infección (pInfect):"),
+            dcc.Input(id='pInfect', type='number', value=0.1, step=0.01),
+            
+            html.Label("Probabilidad de Recuperación (pRecover):"),
+            dcc.Input(id='pRecover', type='number', value=0.01, step=0.01),
+            
+            html.Label("Probabilidad de Muerte (pDeath):"),
+            dcc.Input(id='pDeath', type='number', value=0.005, step=0.001),
+    ])
+    elif selected_model == 'SIRS_period':
+        return html.Div([
+            html.Label("Número de Nodos:"),
+            dcc.Input(id='num-nodes', type='number', value=300, min=10, step=10),
+            
+            html.Label("Probabilidad de Infección (pInfect):"),
+            dcc.Input(id='pInfect', type='number', value=0.1, step=0.01),
+            
+            html.Label("Probabilidad de Recuperación (pRecover):"),
+            dcc.Input(id='pRecover', type='number', value=0.01, step=0.01),
+            
+            html.Label("Número de iteraciones para la recuperación (recovery_duration):"),
+            dcc.Input(id='recovery_duration', type='number', value=5, step=1),
+    ])
+    
+    elif selected_model == 'SIRS_probability':
+        return html.Div([
+            html.Label("Número de Nodos:"),
+            dcc.Input(id='num-nodes', type='number', value=300, min=10, step=10),
+            
+            html.Label("Probabilidad de Infección (pInfect):"),
+            dcc.Input(id='pInfect', type='number', value=0.1, step=0.01),
+            
+            html.Label("Probabilidad de Recuperación (pRecover):"),
+            dcc.Input(id='pRecover', type='number', value=0.01, step=0.01),
+            
+            html.Label("Probabilidad de volver a ser susceptible (pSusceptible):"),
+            dcc.Input(id='pSusceptible', type='number', value=0.05, step=0.01),
+    ])
+    
+    elif selected_model == 'SEIR':
+        return html.Div([
+            html.Label("Número de Nodos:"),
+            dcc.Input(id='num-nodes', type='number', value=300, min=10, step=10),
+            
+            html.Label("Probabilidad de Infección (pInfect):"),
+            dcc.Input(id='pInfect', type='number', value=0.1, step=0.01),
+            
+            html.Label("Probabilidad de Recuperación (pRecover):"),
+            dcc.Input(id='pRecover', type='number', value=0.01, step=0.01),
+            
+            html.Label("Probabilidad de convertirse en infeccioso (pExposedToInfectious):"),
+            dcc.Input(id='pExposedToInfectious', type='number', value=0.05, step=0.01),
+    ])
+    
+    elif selected_model == 'SEIRS_inmunity':
+        return html.Div([
+            html.Label("Número de Nodos:"),
+            dcc.Input(id='num-nodes', type='number', value=300, min=10, step=10),
+            
+            html.Label("Probabilidad de Infección (pInfect):"),
+            dcc.Input(id='pInfect', type='number', value=0.1, step=0.01),
+            
+            html.Label("Probabilidad de Recuperación (pRecover):"),
+            dcc.Input(id='pRecover', type='number', value=0.01, step=0.01),
+            
+            html.Label("Probabilidad de convertirse en infeccioso (pExposedToInfectious):"),
+            dcc.Input(id='pExposedToInfectious', type='number', value=0.05, step=0.01),
+            
+            html.Label("Periodo de tiempo de inmunidad (immunity_period):"),
+            dcc.Input(id='immunity_period', type='number', value=0.05, step=0.01),
+    
+    ])
+    
+    elif selected_model == 'SEIRS_plossimmunity':
+        return html.Div([
+            html.Label("Número de Nodos:"),
+            dcc.Input(id='num-nodes', type='number', value=300, min=10, step=10),
+            
+            html.Label("Probabilidad de Infección (pInfect):"),
+            dcc.Input(id='pInfect', type='number', value=0.1, step=0.01),
+            
+            html.Label("Probabilidad de Recuperación (pRecover):"),
+            dcc.Input(id='pRecover', type='number', value=0.01, step=0.01),
+            
+            html.Label("Probabilidad de convertirse en infeccioso (pExposedToInfectious):"),
+            dcc.Input(id='pExposedToInfectious', type='number', value=0.05, step=0.01),
+            
+            html.Label("Probabilidad de volver a ser susceptible (pSusceptible):"),
+            dcc.Input(id='pSusceptible', type='number', value=0.05, step=0.01),
+  
+    ])
+  
+
+    else:
+        return html.Div() 
+
+
 
 def quarantine_simulation(g, num_quarantine):
     nodes = list(g.nodes)
-    quarantined_nodes = random.sample(nodes, min(num_quarantine, len(nodes)))  # Asegúrate de no exceder los nodos disponibles
+    quarantined_nodes = random.sample(nodes, min(num_quarantine, len(nodes)))  
 
     for node in quarantined_nodes:
         g.nodes[node]['state'] = SPREADING_QUARANTINED
@@ -132,8 +248,6 @@ def vaccinate_nodes(g, num_vaccinate, p_vaccinate):
 
     return g
 
-    return g
-
 @app.callback(
     Output('graph', 'figure'),
     Output('graph-data', 'data'),
@@ -146,118 +260,133 @@ def vaccinate_nodes(g, num_vaccinate, p_vaccinate):
     Input('reset-button', 'n_clicks'),
     Input('quarantine-button', 'n_clicks'),
     Input('disconnect-button', 'n_clicks'),
-    Input('vaccinate-button', 'n_clicks'),  # Nuevo input
+    Input('vaccinate-button', 'n_clicks'),
     Input('model-dropdown', 'value'),
-
     State('num-nodes', 'value'),
     State('pInfect', 'value'),
     State('pRecover', 'value'),
+    State('quarantine-input', 'value'),
+    State('disconnect-input', 'value'),
+    State('vaccination-input', 'value'),
+    State('pVaccinate', 'value'),
+    State('graph-data', 'data'),
+    State('simulation-running', 'data')
+)    
 
-    State('quarantine-input', 'value'),  
-    State('disconnect-input', 'value'),  
-   
-   State('vaccination-input', 'value'),  # Nuevo estado
-   State('pVaccinate', 'value'),  # Nuevo estado
-    
-   State('graph-data', 'data'),
-   State('simulation-running', 'data')
-)
 def update_graph(n_intervals, start_clicks, pause_clicks, continue_clicks,
                   reset_clicks, quarantine_clicks,
                   disconnect_clicks, vaccinate_clicks,
-                  selected_model,
+                  selected_model, 
                   num_nodes, p_infect, p_recover,
                   num_quarantine, num_disconnect,
                   num_vaccinate, p_vaccinate,
                   graph_data, simulation_running):
+    p_death = 0.005
+    recovery_duration=5
+    pSusceptible=0.05
+    pExposedToInfectious=0.05
+    immunity_period=5
     
-   ctx = dash.callback_context
-   triggered_id = ctx.triggered[0]['prop_id'].split('.')[0]
+    ctx = dash.callback_context
+    triggered_id = ctx.triggered[0]['prop_id'].split('.')[0]
 
-   if triggered_id == 'reset-button':
-       return go.Figure(), {'graph': None, 'step': 0}, False, True
+    if triggered_id == 'reset-button':
+        return go.Figure(), {'graph': None, 'step': 0}, False, True
 
-   if triggered_id == 'start-button' and graph_data['graph'] is None:
-       g = nx.erdos_renyi_graph(num_nodes, 0.01)
-       spreading_init(g)
-       spreading_seed(g, 0.05)
-       graph_data['graph'] = nx.node_link_data(g)
-       graph_data['step'] = 1
-       simulation_running = True
-       return dash.no_update, graph_data, True, False  
+    if triggered_id == 'start-button' and graph_data['graph'] is None:
+        g = nx.erdos_renyi_graph(num_nodes, 0.01)
+        spreading_init(g)
+        spreading_seed(g, 0.05)
+        graph_data['graph'] = nx.node_link_data(g)
+        graph_data['step'] = 1
+        simulation_running = True
+        return dash.no_update, graph_data, True, False  
 
-   if graph_data['graph'] is not None:
-       g = nx.node_link_graph(graph_data['graph'])
+    if graph_data['graph'] is not None:
+        g = nx.node_link_graph(graph_data['graph'])
 
-   if triggered_id == 'pause-button':
-       simulation_running = False
-       return dash.no_update, graph_data, simulation_running, True 
+    if triggered_id == 'pause-button':
+        simulation_running = False
+        return dash.no_update, graph_data, simulation_running, True 
 
-   if triggered_id == 'continue-button':
-       simulation_running = True
-       return dash.no_update, graph_data, simulation_running, False 
+    if triggered_id == 'continue-button':
+        simulation_running = True
+        return dash.no_update, graph_data, simulation_running, False 
 
-   if triggered_id == 'quarantine-button':
-       g = quarantine_simulation(g, num_quarantine) 
-       graph_data['graph'] = nx.node_link_data(g)
+    if triggered_id == 'quarantine-button':
+        g = quarantine_simulation(g, num_quarantine) 
+        graph_data['graph'] = nx.node_link_data(g)
 
-   if triggered_id == 'disconnect-button':
-       g = disconnect_random_nodes(g, num_disconnect)  
-       graph_data['graph'] = nx.node_link_data(g)
+    if triggered_id == 'disconnect-button':
+        g = disconnect_random_nodes(g, num_disconnect)  
+        graph_data['graph'] = nx.node_link_data(g)
 
-   if triggered_id == 'vaccinate-button':
-       g = vaccinate_nodes(g, num_vaccinate, p_vaccinate)  
-       graph_data['graph'] = nx.node_link_data(g)
+    if triggered_id == 'vaccinate-button':
+        g = vaccinate_nodes(g, num_vaccinate, p_vaccinate)  
+        graph_data['graph'] = nx.node_link_data(g)
 
-   if simulation_running:
-       if selected_model == 'SIR':
-           model = spreading_make_sir_model(p_infect, p_recover)
-       elif selected_model == 'SIS':
-           model = spreading_make_sis_model(p_infect, p_recover)
+    if simulation_running:
+        if selected_model == 'SIR':
+            model = spreading_make_sir_model(p_infect, p_recover)
+        elif selected_model == 'SIS':
+            model = spreading_make_sis_model(p_infect, p_recover)
+        elif selected_model == 'SIRD':
+            model = spreading_make_sird_model(p_infect, p_recover, p_death)
+        elif selected_model == 'SIRS_period':
+            model = spreading_make_sirs_model(p_infect, p_recover, recovery_duration)
+        elif selected_model == 'SIRS_probability':
+            model = spreading_make_sirs_model(p_infect, p_recover, pSusceptible)
+        elif selected_model == 'SEIR':
+            model = spreading_make_seir_model(p_infect, pExposedToInfectious, p_recover)
+        elif selected_model == 'SEIRS_inmunity':
+            model = spreading_make_seirs_model(p_infect, pExposedToInfectious,p_recover, immunity_period)
+        elif selected_model == 'SEIRS_plossimmunity':
+            model = spreading_make_seirs_model(p_infect, pExposedToInfectious,p_recover, pSusceptible)
 
-       spreading_step(g, model)
-       graph_data['graph'] = nx.node_link_data(g)
-       graph_data['step'] += 1
 
-   pos = nx.spring_layout(g, k=0.6)
-   
-   node_trace = go.Scatter(
-       x=[pos[i][0] for i in g.nodes],
-       y=[pos[i][1] for i in g.nodes],
-       mode='markers',
-       marker=dict(
-           color=[color_map[g.nodes[i]['state']] for i in g.nodes],
-           size=10,
-           line=dict(width=2)
-       )
-   )
-   
-   edge_x = []
-   edge_y = []
-   
-   for edge in g.edges():
-       x0, y0 = pos[edge[0]]
-       x1, y1 = pos[edge[1]]
-       edge_x += [x0, x1, None]
-       edge_y += [y0, y1, None]
+        spreading_step(g, model)
+        graph_data['graph'] = nx.node_link_data(g)
+        graph_data['step'] += 1
 
-   edge_trace = go.Scatter(
-       x=edge_x,
-       y=edge_y,
-       line=dict(width=1,color='black'),
-       hoverinfo='none',
-       mode='lines'
-   )
+    pos = nx.spring_layout(g, k=0.6)
+    
+    node_trace = go.Scatter(
+        x=[pos[i][0] for i in g.nodes],
+        y=[pos[i][1] for i in g.nodes],
+        mode='markers',
+        marker=dict(
+            color=[color_map[g.nodes[i]['state']] for i in g.nodes],
+            size=10,
+            line=dict(width=2)
+        )
+    )
+    
+    edge_x = []
+    edge_y = []
+    
+    for edge in g.edges():
+        x0, y0 = pos[edge[0]]
+        x1, y1 = pos[edge[1]]
+        edge_x += [x0, x1, None]
+        edge_y += [y0, y1, None]
 
-   fig = go.Figure(data=[edge_trace,node_trace])
-   
-   fig.update_layout(
-      showlegend=False,
-      xaxis=dict(showgrid=False , zeroline=False),
-      yaxis=dict(showgrid=False , zeroline=False)
-   )
+    edge_trace = go.Scatter(
+        x=edge_x,
+        y=edge_y,
+        line=dict(width=1,color='black'),
+        hoverinfo='none',
+        mode='lines'
+    )
 
-   return fig , graph_data , simulation_running , False  
+    fig = go.Figure(data=[edge_trace,node_trace])
+    
+    fig.update_layout(
+        showlegend=False,
+        xaxis=dict(showgrid=False , zeroline=False),
+        yaxis=dict(showgrid=False , zeroline=False)
+    )
+
+    return fig , graph_data , simulation_running , False  
 
 if __name__ == '__main__':
    app.run_server(debug=True)
